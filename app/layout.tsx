5350cf0a16a39d20c100d476a97f810c4d00615e
Voramate Kittichotirut <voramate05@outlook.co.th>
2026-02-19 01:10:54 +0700

    feat: Add OpenGraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import type {Metadata} from "next";
+import type {Metadata, Viewport} from "next";
 import {Geist, Geist_Mono, Noto_Sans_Thai, Bai_Jamjuree, Roboto} from "next/font/google";
 import localFont from 'next/font/local'
 import "./globals.css";
 import {Navbar} from "@/components/ui/navbar";
 import {Footer} from "@/components/ui/footer";
+import { Toaster } from "@/components/ui/sonner"
 import MicrosoftClarity from "@/app/metrics/MicrosoftClarity";
+import {UserProvider} from "@/contexts/UserContext";
 
 export const runtime = 'edge';
 
@@ -41,12 +43,45 @@ const geistMono = Geist_Mono({
     subsets: ["latin"],
 });
 
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    // This helps content flow behind the status bar if needed
+    viewportFit: 'cover',
+}
+
 export const metadata: Metadata = {
     title: {
-        template: '%s | ComCamp37',
-        default: 'ComCamp37',
+        template: '%s | ComCamp 37',
+        default: 'ComCamp 37',
     },
-    description: "ค่าย ComCamp37 จากวิศวะคอมฯ บางมด ขอเชิญน้อง ๆ ม.ปลาย มาปลดล็อกสกิลในค่ายสุดเจ๋งที่มาพร้อมกับความรู้และความสนุก สมัครฟรี เตรียมตัวให้พร้อมแล้วมาร่วมผจญภัยไปด้วยกัน!",
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: 'black-translucent',
+    },
+    description: "กลับมาอีกครั้ง ! กับค่าย “ComCamp 37” จัดโดยพี่ ๆ วิศวะคอมฯ บางมด ขอเชิญน้อง ๆ มาร่วมกอบกู้เมืองที่ทุกคนมีความฝัน ใครพร้อมแล้ว ? สมัครเลย ! งานนี้ ได้ทั้งความรู้ ความสนุก ฟรีตลอดค่าย",
+    openGraph: {
+        title: 'ComCamp 37 ค่ายวิศวะคอมพิวเตอร์ มจธ.',
+        description: 'กลับมาอีกครั้ง ! กับค่าย “ComCamp 37” จัดโดยพี่ ๆ วิศวะคอมฯ บางมด ขอเชิญน้อง ๆ มาร่วมกอบกู้เมืองที่ทุกคนมีความฝัน ใครพร้อมแล้ว ? สมัครเลย ! งานนี้ ได้ทั้งความรู้ ความสนุก ฟรีตลอดค่าย',
+        url: 'https://comcamp.io',
+        siteName: 'ComCamp 37',
+        images: [
+            {
+                url: 'https://storage.comcamp.io/web-assets/og.png',
+                width: 1200,
+                height: 630,
+                alt: 'ComCamp 37 - Zootopia Theme City Rebuilding',
+            },
+        ],
+        locale: 'th_TH',
+        type: 'website',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'ComCamp 37',
+        description: 'ค่ายคอมพิวเตอร์วิศวะบางมด กลับมาแล้ว!',
+        images: ['https://storage.comcamp.io/web-assets/og.png'],
+    },
 };
 
 export default function RootLayout({
@@ -57,11 +92,15 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body
-            className={`${notoSansThai.variable} ${zootopiaFont.variable} ${baiJamjuree.variable} ${roboto.variable} font-bai_jamjuree antialiased bg-[#1F456E] dark`} /* bg-[#2D364E] #232C40 */
+            className={`${notoSansThai.variable} ${zootopiaFont.variable} ${baiJamjuree.variable} ${roboto.variable} font-bai_jamjuree antialiased bg-theme-primary dark`} /* bg-[#2D364E] #232C40 */
         >
-        { process.env.NEXT_PUBLIC_IS_COMINGSOON  == 'false' && (<Navbar/>)}
+        <UserProvider>
+            { process.env.NEXT_PUBLIC_IS_COMINGSOON  == 'false' && (<Navbar/>)}
+
         {children}
+        </UserProvider>
         <MicrosoftClarity/>
+        <Toaster />
         </body>
         </html>
     );
